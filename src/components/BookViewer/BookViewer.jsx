592c1180461a4c7a5274e8352c02e039a900631c
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-08-10 15:27:28 -0500

    split bookview logic to bookviewer

---
+++ src/components/BookViewer/BookViewer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const BookViewer = (props) => {
+    return (
+        <div className="row row-spacer">
+            <div className="col-md-4">
+                {/**Button will be here for previous book */}
+                <button onClick={props.previousBook}>Previous Book</button>
+            </div>
+            <div className="col-md-4">
+                {/**Book will be displayed with cover here */}
+                <h1>{props.book.title}</h1>
+                <h4>{props.book.author}</h4>
+            </div>
+            <div className="col-md-4">
+                {/**Button will be here for next book */}
+                <button onClick={props.nextBook}>Next Book</button>
+            </div>
+        </div>
+      );
+}
+ 
+export default BookViewer;
